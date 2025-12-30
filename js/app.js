d29dcc9e4a424ca6e4d52b2271b6c251702b0bb7
Behruz <behruzsadullayev0913@gmail.com>
2025-12-30 13:31:04 +0500

    h

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Obyektlar
+const icome = document.getElementById('.total-income')
+const expense = document.getElementById('.total-expense')
+const balance = document.getElementById('.total-balance')
+const
+const
+const
+const
+const
