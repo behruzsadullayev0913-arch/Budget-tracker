bfa9a63d2693fd677e102b3c1c5c1ed5c2f6c9bc
Behruz <behruzsadullayev0913@gmail.com>
2025-12-30 14:23:04 +0500

    css tugadi!

---
+++ js/api.js
@@ -0,0 +1,15 @@
+import { storage } from "./storage.js";
+
+export const api = {
+  async fetchData() {
+    return new Promise((resolve, reject) => {
+      const data = storage.load();
+
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Ma'lumotlarni yuklashda xatolik yuz berdi!");
+      }
+    });
+  },
+};
